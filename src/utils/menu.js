0ba3d563532dc4e27bc8b09ea2951d364e5d1bf9
elivoa <elivoa@gmail.com>
2017-06-16 10:17:43 +0800

    merge

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,6 +1,7 @@
 module.exports = [
   { id: 1, icon: 'laptop', name: '首页', router: '/' },
   { id: 2, bpid: 1, name: '学会活动', icon: 'api', router: '/seminar' },
+
   { id: 3, bpid: 1, name: '专业委员会', icon: 'api', router: '/technical-committees' },
 
   { id: 4, bpid: 1, name: '设置', icon: 'camera-o' },
@@ -11,6 +12,7 @@ module.exports = [
   { id: 6, bpid: 1, name: '专家地图', icon: 'environment', router: '/expert-map' },
   { id: 8, bpid: 1, name: '隐藏功能', icon: 'camera-o' },
   { id: 88, bpid: 8, mpid: 8, name: 'TEST', icon: 'user', router: '/hidden/testpage' },
+
 
   // {
   //   id: 41,

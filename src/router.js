605608213a35b8d7e42110d6e1ef95c9d90e3b17
elivoa <elivoa@gmail.com>
2017-07-10 18:01:37 +0800

    Enterprise 系统的一些提交.
    Signed-off-by: elivoa <elivoa@gmail.com>

--- src/router.js
+++ src/router.js
@@ -220,7 +220,17 @@ const Routers = function ({ history, app }) {
         require.ensure([], (require) => {
           registerModel(app, require('./models/vis/vis-research-interest'));
           cb(null, { component: require('./routes/lab/KnoledgeGraphWidget') });
-        }, 'lab/knowledge-graph-widget');
+        }, 'knowledge-graph');
+      },
+    },
+    {
+      path: '/knowledge-graph',
+      component: App,
+      getIndexRoute(nextState, cb) {
+        require.ensure([], (require) => {
+          registerModel(app, require('./models/vis/vis-research-interest'));
+          cb(null, { component: require('./routes/KnowledgeGraphPage') });
+        }, 'knowledge-graph');
       },
     },
     {
@@ -241,132 +251,7 @@ const Routers = function ({ history, app }) {
         }, 'error');
       },
     },
-
-    // examples ----------------------------------------------------------------------
-
-    {
-      path: '/dashboard',
-      component: App,
-      getIndexRoute(nextState, cb) {
-        require.ensure([], require => {
-          registerModel(app, require('./models/dashboard'));
-          cb(null, { component: require('./routes/dashboard/') });
-        }, 'dashboard');
-      },
-      childRoutes: [
-        {
-          path: '',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              registerModel(app, require('./models/dashboard'));
-              cb(null, require('./routes/dashboard/'));
-            }, 'dashboard');
-          },
-        }, {
-          path: 'user',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              registerModel(app, require('./models/user'));
-              cb(null, require('./routes/user/'));
-            }, 'user');
-          },
-        }, {
-          path: 'user/:id',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              registerModel(app, require('./models/user/detail'));
-              cb(null, require('./routes/user/detail/'));
-            }, 'user-detail');
-          },
-        }, {
-          path: 'login',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              registerModel(app, require('./models/login'));
-              cb(null, require('./routes/login/'));
-            }, 'login');
-          },
-        }, {
-          path: 'request',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/request/'));
-            }, 'request');
-          },
-        }, {
-          path: 'UIElement/iconfont',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/iconfont/'));
-            }, 'UIElement-iconfont');
-          },
-        }, {
-          path: 'UIElement/search',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/search/'));
-            }, 'UIElement-search');
-          },
-        }, {
-          path: 'UIElement/dropOption',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/dropOption/'));
-            }, 'UIElement-dropOption');
-          },
-        }, {
-          path: 'UIElement/layer',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/layer/'));
-            }, 'UIElement-layer');
-          },
-        }, {
-          path: 'UIElement/dataTable',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/dataTable/'));
-            }, 'UIElement-dataTable');
-          },
-        }, {
-          path: 'UIElement/editor',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/UIElement/editor/'));
-            }, 'UIElement-editor');
-          },
-        }, {
-          path: 'chart/lineChart',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/chart/lineChart/'))
-            }, 'chart-lineChart')
-          },
-        }, {
-          path: 'chart/barChart',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/chart/barChart/'))
-            }, 'chart-barChart')
-          },
-        }, {
-          path: 'chart/areaChart',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/chart/areaChart/'));
-            }, 'chart-areaChart');
-          },
-        }, {
-          path: '*',
-          getComponent(nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/error/'));
-            }, 'error');
-          },
-        },
-      ],
-    },
-  ]
+  ];
 
   return <Router history={history} routes={routes} />;
 }

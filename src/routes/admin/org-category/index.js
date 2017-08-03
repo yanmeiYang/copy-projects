c7153ccd99d83d19cd2c38c0ed4a200af8a270d5
elivoa <elivoa@gmail.com>
2017-08-04 02:58:21 +0800

    change org-list to vertical
    Signed-off-by: elivoa <elivoa@gmail.com>

--- src/routes/admin/org-category/index.js
+++ src/routes/admin/org-category/index.js
@@ -6,55 +6,80 @@ import { Tabs } from 'antd';
 import { connect } from 'dva';
 import UniversalConfig from '../../common/universal-config/index';
 import styles from './index.less';
+import { classnames } from '../../../utils';
 
 
 const TabPane = Tabs.TabPane;
 
 class OrgCategory extends React.Component {
-
   /** 在Component被加载的时候调用的。 */
   componentDidMount() {
-    this.props.dispatch(
-      {
-        type: 'universalConfig/setTabList',
-        payload: { category: 'orgcategory' },
-      });
-  };
+    this.props.dispatch({
+      type: 'universalConfig/setTabList',
+      payload: { category: 'orgcategory' },
+    });
+  }
 
   onTabChange = (activeKey) => {
-    console.log(activeKey);
-    this.props.dispatch(
-      {
-        type: 'universalConfig/setCategory',
-        payload: { category: activeKey },
-      });
+    this.props.dispatch({
+      type: 'universalConfig/setCategory',
+      payload: { category: activeKey },
+    });
+  };
+
+  onAddOrgList = () => {
+    console.log('onAddOrgList');
+  };
+
+  onEditOrgList = () => {
+    console.log('onAddOrgList');
+  };
+
+  onDeleteOrgList = () => {
+    console.log('onAddOrgList');
   };
 
   render() {
-    const tabData = this.props.universalConfig.tabList;
+    const { tabList } = this.props.universalConfig;
+    console.log('tabList', tabList);
     return (
-      <div style={{ paddingLeft: 24 }}>
-        <h2 style={{ marginBottom: 12 }}>机构列表</h2>
-        {tabData.length > 0 &&
-        <Tabs
-          defaultActiveKey={tabData[0].category}
-          type="card"
-          className={styles.tabs}
-          onChange={this.onTabChange}
-        >
-          {tabData.map((item) => {
-            return (
-              <TabPane
-                key={item.category}
-                tab={item.name}
-                className={styles.tabContent}
-              >
-              </TabPane>
-            );
-          })}
-        </Tabs>
-        }
-        <UniversalConfig hideValue />
+      <div className={classnames('', styles.page)}>
+        <h2>机构列表</h2>
+
+        <div className={styles.main}>
+          <div className={styles.left}>
+
+            <div className={styles.toolbox}>
+              <a onClick={this.onAddOrgList}>添加</a>
+              <span className="spliter">|</span>
+              <a onClick={this.onEditOrgList}>编辑</a>
+              <span className="spliter">|</span>
+              <a onClick={this.onDeleteOrgList}>删除</a>
+            </div>
+
+            {tabList &&
+            <Tabs
+              type="line" size="small"
+              tabPosition="left"
+              onChange={this.onTabChange}
+            >
+              {tabList.map((item) => {
+                return (
+                  <TabPane
+                    key={item.category}
+                    tab={item.name}
+                  />
+                );
+              })}
+            </Tabs>
+            }
+          </div>
+
+          <div className={styles.right}>
+            <UniversalConfig hideValue />
+          </div>
+
+        </div>
       </div>
     );
   }

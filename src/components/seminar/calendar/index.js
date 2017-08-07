6109c60970f60ec4114cc6b5d5c28dfe866ee3c7
冉沿川 <zenensen@ranyanchuandeMacBook-Pro.local>
2017-08-07 11:26:06 +0800

    calendar center

--- src/components/seminar/calendar/index.js
+++ src/components/seminar/calendar/index.js
@@ -4,12 +4,13 @@
 import React from 'react';
 import moment from 'moment';
 import { DatePicker } from 'antd';
+import styles from './index.less';
 
 const { RangePicker } = DatePicker;
 class CanlendarInForm extends React.Component {
   state = {
     startValue: `${(new Date()).format('yyyy-MM-dd')} 09:00`,
-    endValue: `${(new Date()).format('yyyy-MM-dd')} 09:00`,
+    endValue: `${(new Date()).format('yyyy-MM-dd')} 18:00`,
   };
 
   componentWillReceiveProps(nextProps) {
@@ -33,15 +34,15 @@ class CanlendarInForm extends React.Component {
 
   render() {
     const { startValue, endValue } = this.state;
-    const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+    const dateFormat = 'YYYY-MM-DD HH:mm';
     return (
       <div>
         {startValue && endValue &&
         <RangePicker
-          showTime={{ format: 'HH:mm' }}
-          format="YYYY-MM-DD HH:mm"
+          className={styles.calendar}
+          showTime={dateFormat}
+          format={dateFormat}
           placeholder={['开始时间', '结束时间']}
-          // value={[startValue,endValue]}
           defaultValue={[moment(startValue, dateFormat), moment(endValue, dateFormat)]}
           onChange={this.onChange}
           onOk={this.onOk}

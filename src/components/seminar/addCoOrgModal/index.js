421517ba47ab3cf19c383ced5ddbff237b6702b3
zhanglm <zhanglimin2016@icloud.com>
2017-08-29 17:13:15 +0800

    协办单位组件格式化的修正

--- src/components/seminar/addCoOrgModal/index.js
+++ src/components/seminar/addCoOrgModal/index.js
@@ -3,32 +3,32 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Modal, Cascader, Input, Row, Col, Button, Tag} from 'antd';
-import {contactByJoint, getValueByJoint} from '../../../services/seminar';
+import { Modal, Cascader, Input, Row, Col, Button, Tag } from 'antd';
+import { contactByJoint, getValueByJoint } from '../../../services/seminar';
 import styles from './index.less';
 
 class AddCoOrgModal extends React.Component {
-  state = {modalVisible: false, currentOrg: [], tags: []};
+  state = { modalVisible: false, currentOrg: [], tags: [] };
 
   componentDidMount() {
     if (this.props.coOrg !== '') {
-      this.setState({tags: this.props.coOrg, currentOrg: this.props.coOrg});
+      this.setState({ tags: this.props.coOrg, currentOrg: this.props.coOrg });
     }
   }
 
   onOrgChange = (value) => {
     if (value[1]) {
       const data = contactByJoint(value[0], value[1]);
-      this.setState({currentOrg: this.state.currentOrg.concat(data)});
+      this.setState({ currentOrg: this.state.currentOrg.concat(data) });
     }
   };
 
   setModalVisible = () => {
-    this.setState({modalVisible: false, manual: false});
+    this.setState({ modalVisible: false, manual: false });
   };
   deleteTag = (org, index, e) => {
     const tags = this.state.currentOrg.filter(tag => tag !== org);
-    this.setState({currentOrg: tags, tags});
+    this.setState({ currentOrg: tags, tags });
     this.props.callbackParent(tags);
   };
   addOrg = (e) => {
@@ -46,9 +46,9 @@ class AddCoOrgModal extends React.Component {
       }
     });
     if (isExist.length <= 0 && value !== '') {
-      const data = {key: value, val: ' '};
-      this.props.dispatch({type: 'seminar/addKeyAndValue', payload: data});
-      this.setState({currentOrg: value === '' ? this.state.currentOrg : this.state.currentOrg.concat(value)});
+      const data = { key: value, val: ' ' };
+      this.props.dispatch({ type: 'seminar/addKeyAndValue', payload: data });
+      this.setState({ currentOrg: value === '' ? this.state.currentOrg : this.state.currentOrg.concat(value) });
     } else if (isExist.length > 0) {
       this.setState({
         currentOrg: this.state.currentOrg.concat(contactByJoint(parent.value, isExist[0].value)),
@@ -60,24 +60,24 @@ class AddCoOrgModal extends React.Component {
     if (this.state.manual) {
       ReactDOM.findDOMNode(this.refs.manualValue).value = '';
     }
-    this.setState({modalVisible: false, tags: this.state.currentOrg});
+    this.setState({ modalVisible: false, tags: this.state.currentOrg });
     this.props.callbackParent(this.state.currentOrg);
   };
   showAddCoOrgModal = () => {
-    this.setState({modalVisible: true});
+    this.setState({ modalVisible: true });
   };
 
   jumpToManual = () => {
-    const {tags, currentOrg} = this.state;
+    const { tags, currentOrg } = this.state;
     if (currentOrg.length > tags.length) {
       currentOrg.pop();
     }
-    this.setState({manual: !this.state.manual, currentOrg});
+    this.setState({ manual: !this.state.manual, currentOrg });
   };
 
   render() {
-    const {orgList} = this.props;
-    const {modalVisible, tags, manual} = this.state;
+    const { orgList } = this.props;
+    const { modalVisible, tags, manual } = this.state;
     return (
       <div>
         {tags.map((org, index) => {
@@ -101,20 +101,21 @@ class AddCoOrgModal extends React.Component {
           onCancel={this.setModalVisible.bind()}
         >
           <div>
-            <Row style={{maxHeight: '555vh'}}>
+            <Row style={{ maxHeight: '555vh' }}>
               <Col span={4} className={styles.label}>
                 选择协办单位:
               </Col>
               <Col span={20}>
                 <Cascader options={orgList} onChange={this.onOrgChange} ref="cascader"
-                          showSearch placeholder="请键入搜索协办单位" popupClassName={styles.addAssistMenu} style={{width: '100%'}}/>
+                          showSearch placeholder="请键入搜索协办单位" popupClassName={styles.addAssistMenu}
+                          style={{ width: '100%' }}/>
               </Col>
             </Row>
-            <Row style={{ marginTop: '10px'}}>
+            <Row style={{ marginTop: '10px' }}>
               <Col span={4} className={styles.label}>手动填写:</Col>
               <Col span={20}> <Input onBlur={this.addOrg} ref="manualValue"/></Col>
               <Col span={24} className={styles.action}>
-                <Button type="primary"  onClick={this.handleOk}>提交</Button>
+                <Button type="primary" onClick={this.handleOk}>提交</Button>
               </Col>
             </Row>
           </div>

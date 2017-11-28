4024e3bf3eb48c7c457c7b89da6afc7ccc84548f
Yanmei Yang <hd_ym@qq.com>
2017-11-28 11:51:38 +0800

    一路点的bug

--- src/routes/search/SearchComponent.js
+++ src/routes/search/SearchComponent.js
@@ -262,7 +262,7 @@ export default class SearchComponent extends Component {
               title={Math.random()}
               filters={filters}
               aggs={aggs}
-              roles={this.props.app.roles.role}
+              roles={this.props.app.roles && this.props.app.roles.role}
               onFilterChange={this.onFilterChange}
               onExpertBaseChange={this.onExpertBaseChange}
               disableExpertBaseFilter={disableExpertBaseFilter}

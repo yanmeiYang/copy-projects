8e3496b0f06fa3b902d3e0310309714a2a08e402
Unknown <yebeixiaohai@gmail.com>
2017-11-29 19:41:41 +0800

    modify feedback

--- src/components/Layout/Feedback.js
+++ src/components/Layout/Feedback.js
@@ -11,17 +11,20 @@ const FormItem = Form.Item;
 const { TextArea } = Input;
 
 class Feedback extends React.Component {
+  state = {
+    visible: false,
+  };
 
-  // componentDidUpdate(prevProps) {
-  //   console.log('===========', prevProps.feedbackStatus);
-  //   console.log('>>>>>>>>>>>>>>>>====', this.props.feedbackStatus);
-  //   if (prevProps.feedbackStatus && prevProps.feedbackStatus !== this.props.feedbackStatus) {
-  //     this.props.form.resetFields();
-  //   }
-  // }
+// componentDidUpdate(prevProps) {
+//   console.log('===========', prevProps.feedbackStatus);
+//   console.log('>>>>>>>>>>>>>>>>====', this.props.feedbackStatus);
+//   if (prevProps.feedbackStatus && prevProps.feedbackStatus !== this.props.feedbackStatus) {
+//     this.props.form.resetFields();
+//   }
+// }
 
   componentWillReceiveProps(nextProps) {
-    if ( nextProps.feedbackStatus && this.props.feedbackStatus !== nextProps.feedbackStatus) {
+    if (nextProps.feedbackStatus && this.props.feedbackStatus !== nextProps.feedbackStatus) {
       this.props.form.resetFields();
     }
   }
@@ -29,6 +32,7 @@ class Feedback extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
+      console.log('d>>>>>>>>>>>>', values)
       if (!err) {
         const data = values;
         this.props.dispatch({
@@ -38,6 +42,28 @@ class Feedback extends React.Component {
       }
     });
   };
+  closePopover = () => {
+    this.setState({
+      visible: false
+    })
+  };
+  openPopover = () => {
+    this.setState({
+      visible: true
+    })
+  };
+  mouseLeave = () => {
+    let values = this.props.form.getFieldsValue()
+    console.log('dddddd00000000', values, values.email)
+    if (!values.email && !values.content) {
+      this.setState({
+        visible: false,
+      })
+    } else {
+      console.log('right')
+    }
+
+  };
 
   render() {
     const { getFieldDecorator, validateFieldsAndScroll } = this.props.form;
@@ -45,13 +71,15 @@ class Feedback extends React.Component {
     const load = this.props.loading.effects['app/setFeedback'];
 
     const content = (
-      <Form onSubmit={this.handleSubmit} className={styles.feedbackForm}>
+      <Form onSubmit={this.handleSubmit} className={styles.feedbackForm}
+            onMouseLeave={this.mouseLeave}>
+        <span>请输入您的email和问题或建议,我们会及时的处理你的宝贵意见：</span>
         <FormItem
         >
           {getFieldDecorator('email', {
             rules: [{ type: 'email', message: '邮箱格式错误!' }],
           })(
-            <Input placeholder="Email" />,
+            <Input placeholder="请输入您的email !" className={styles.inputBox} />,
           )}
         </FormItem>
         <FormItem
@@ -59,7 +87,7 @@ class Feedback extends React.Component {
           {getFieldDecorator('content', {
             rules: [{ required: true, message: 'Please input your content' }],
           })(
-            <TextArea rows={4} placeholder="Content" />,
+            <TextArea rows={4} placeholder="请输入您的宝贵意见和建议!" />,
           )}
         </FormItem>
         <FormItem className={styles.submitBtn}>
@@ -73,10 +101,18 @@ class Feedback extends React.Component {
         </FormItem>
       </Form>
     );
+    const title = (
+      <div className={styles.titleBox}>
+        <span>Feedback</span>
+        <i class="fa fa-times" onClick={this.closePopover}> </i>
+      </div>
+    );
     return (
       <Affix offsetBottom={20} className={styles.affixFeedback}>
-        <Popover placement="topRight" content={content} title="Feedback" trigger="hover">
-          <Button icon="fa-smile-o" type="primary" loading={load}>
+        <Popover placement="topRight" content={content}
+                 title={title} visible={this.state.visible}>
+          <Button icon="fa-smile-o" type="primary" loading={load} size="large"
+                  onMouseOver={this.openPopover}>
             {!load &&
             <span>
               <i className="fa fa-smile-o" style={{ fontWeight: 'bold' }} /> &nbsp;
@@ -88,6 +124,7 @@ class Feedback extends React.Component {
     );
   }
 }
+
 export default connect(({ app, loading }) => ({
   user: app.user, feedbackStatus: app.feedbackStatus, loading,
 }))(Form.create()(Feedback));

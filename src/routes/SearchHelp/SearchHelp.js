1b4032fbf792fd7ecbe08740f11cac42e07ac1d8
dinglihua <aliceding19900924@gmail.com>
2017-11-30 17:40:21 +0800

    SearchHelp Modified

--- src/routes/SearchHelp/SearchHelp.js
+++ src/routes/SearchHelp/SearchHelp.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { routerRedux } from 'dva/router';
+import PropTypes from 'prop-types';
 import { connect } from 'dva';
 import { sysconfig } from 'systems';
 import { Checkbox, Row, Col } from 'antd';
+import { Button } from 'antd';
 import styles from './SearchHelp.less';
 
 const CheckboxGroup = Checkbox.Group;
@@ -12,228 +14,184 @@ export default class SearchHelp extends Component {
     defaultExpansionChecked: 1,
     currentExpansionChecked: 0,
     currentTranslationChecked: 0,
-
-    /// unused
-    labels: [
-      {
-        id: 1,
-        expansion: 'Artificial Intelligencesdfdsfsd',
-        translated: '人工智能dfsfsfsf',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }],
-      },
-      {
-        id: 2,
-        expansion: 'check2dsfsdfsdf',
-        translated: '柴可2',
-        superordinateWord: [{ label: 'Sff1' }, { label: 'Sff2' }],
-        subordinateWord: [{ label: 'D31' }, { label: 'd34' }, { label: 'd32' }],
-      },
-      {
-        id: 3,
-        expansion: 'check3dfsddsfsffsfsfs',
-        translated: '柴可3fsdfdsf',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }, { label: 'Dx1' }, { label: 'dx2' }, { label: 'dx2' }],
-      },
-      {
-        id: 4,
-        expansion: 'Artificial Intelligence',
-        translated: '人工智能',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }],
-      },
-      {
-        id: 5,
-        expansion: 'check2',
-        translated: '柴可2',
-        superordinateWord: [{ label: 'Sff1' }, { label: 'Sff2' }],
-        subordinateWord: [{ label: 'D31' }, { label: 'd34' }, { label: 'd32' }],
-      },
-      {
-        id: 6,
-        expansion: 'check3sfdsfgrfsdfsewwer',
-        translated: '柴可3',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }, { label: 'Dx1' }, { label: 'dx2' }, { label: 'dx2' }],
-      },
-      {
-        id: 7,
-        expansion: 'Artificial Intelligence',
-        translated: '人工智能',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }],
-      },
-      {
-        id: 8,
-        expansion: 'check2',
-        translated: '柴可2',
-        superordinateWord: [{ label: 'Sff1' }, { label: 'Sff2' }],
-        subordinateWord: [{ label: 'D31' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }],
-      },
-      {
-        id: 9,
-        expansion: 'check3',
-        translated: '柴可3',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }, { label: 'Dx1' }, { label: 'dx2' }, { label: 'dx2' }],
-      },
-      {
-        id: 10,
-        expansion: 'Artificial Intelligence',
-        translated: '人工智能',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }],
-      },
-      {
-        id: 11,
-        expansion: 'check2',
-        translated: '柴可2',
-        superordinateWord: [{ label: 'Sff1' }, { label: 'Sff2' }],
-        subordinateWord: [{ label: 'D31' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }, { label: 'd34' }, { label: 'd32' }],
-      },
-      {
-        id: 12,
-        expansion: 'Artificial Intelligence',
-        translated: '人工智能',
-        superordinateWord: [{ label: 'S1' }, { label: 'S2' }],
-        subordinateWord: [{ label: 'D1' }, { label: 'd2' }, { label: 'd2' }],
-      },
-    ],
-    ///
+    //
     indeterminate: true,
     checkAll: false,
-    plainOptions: [],
     checkedList: [],
 
     // to delete
     knowledgeGraphId: 0,
+    KG: [],
   };
 
-  onExpandedTermChange = (id) => {
+  onExpandedTermChange = (index) => {
     this.setState({
-      currentExpansionChecked: id,
-      currentTranslationChecked: this.state.currentTranslationChecked === 0 ? 0 : id,
-      defaultExpansionChecked: 0,
+      currentExpansionChecked: index + 1,
+      currentTranslationChecked: this.state.currentTranslationChecked === 0 ? 0 : index + 1,
+      defaultExpansionChecked: null,
     });
+    const { intelligenceSuggests } = this.props;
+    if (this.props.onIntelligenceSearchMetaChange) {
+      this.props.onIntelligenceSearchMetaChange({
+        // expansion: intelligenceSuggests.expandedTexts[index],
+        // translated: this.state.currentTranslationChecked === 0 ? '' : intelligenceSuggests.expandedTexts_zh[index],
+      });
+    }
   };
 
-  onTranslationChange = (id) => {
+  onTranslationChange = (index) => {
     this.setState({
-      currentTranslationChecked: this.state.currentTranslationChecked === id ? 0 : id,
-      currentExpansionChecked: id,
+      currentTranslationChecked: this.state.currentTranslationChecked === index + 1 ? 0 : index + 1,
+      currentExpansionChecked: index + 1,
     });
+    const { intelligenceSuggests } = this.props.intelligenceSuggests;
+    if (this.props.onIntelligenceSearchMetaChange) {
+      this.props.onIntelligenceSearchMetaChange({
+        // expansion: this.state.currentExpansionChecked === 0 ? '' : intelligenceSuggests.expandedTexts[index],
+        // translated: intelligenceSuggests.expandedTexts_zh[index],
+      });
+    }
   };
 
   onKGChange = (checkedList) => {
-    console.log('>> ', checkedList);
+    const { intelligenceSuggests } = this.props;
+    const len = intelligenceSuggests.kgHyponym && intelligenceSuggests.kgHyponym.length;
     this.setState({
       checkedList,
-      indeterminate: !!checkedList.length && (checkedList.length < plainOptions.length),
-      checkAll: checkedList.length === plainOptions.length,
+      indeterminate: !!checkedList.length && (checkedList.length < len),
+      checkAll: checkedList.length === len,
     });
   };
 
   onCheckAllChange = (e) => {
+    const kgHyponymArray = [];
+    const { intelligenceSuggests } = this.props;
+    kgHyponymArray.push(intelligenceSuggests.kgHyponym.map((item) => {
+      return item.word;
+    }));
     this.setState({
-      checkedList: e.target.checked ? this.state.labels : [],
+      checkedList: e.target.checked ? kgHyponymArray[0] : [],
       indeterminate: false,
       checkAll: e.target.checked,
     });
+    if (this.props.onIntelligenceSearchMetaChange) {
+      this.props.onIntelligenceSearchMetaChange({
+        expansion: this.state.currentExpansionChecked === 0 ? '' : intelligenceSuggests.expandedTexts[index],
+        translated: this.state.currentTranslationChecked === 0 ? '' : intelligenceSuggests.expandedTexts_zh[index],
+        KG: kgHyponymArray,
+      });
+    }
   };
-  onToggleClick = (e) => {
-  }
+  //
+  // onToggleClick = (e) => {
+  // };
 
   setDefaultOptions = () => {
     // TODO
   };
 
   render() {
-    const { labels, currentExpansionChecked, currentTranslationChecked, defaultExpansionChecked } = this.state;
+    const {
+      currentExpansionChecked, currentTranslationChecked,
+      defaultExpansionChecked, checkedList,
+    } = this.state;
 
-    if (!labels || labels.length <= 0) {
+    const { intelligenceSuggests } = this.props;
+    if (!intelligenceSuggests) {
       return false;
     }
+    const { expandedTexts, expandedTexts_zh, expands } = intelligenceSuggests;
+    const hasExpansion = expandedTexts && expandedTexts.length > 0;
+    const hasTranslation = expandedTexts_zh && expandedTexts_zh.length > 0;
 
-    // super&sub
-    const curr = labels.filter(item => item.id === (defaultExpansionChecked || currentExpansionChecked));
-    const superOW = curr && curr.length > 0 && curr[0];
-    const { superordinateWord, subordinateWord } = superOW;
-
-    const hasExpansion = labels.find(label => label.expansion) !== undefined;
+    const { kgHypernym, kgHyponym } = intelligenceSuggests;
+    const hasKG = kgHypernym && kgHypernym.length && kgHyponym && kgHyponym.length > 0;
 
     // -----------------------------
     const { knowledgeGraphId } = this.state;
     const { indeterminate } = this.state;
     const { checkAll } = this.state;
 
+    // const hasKG = this.state.KG && this.state.KG.length > 0;
+
     return (
       <div className={styles.searchHelp}>
         <div className={styles.box}>
           <div className={styles.w}>
             {hasExpansion &&
-            <div className={styles.w} >We automatically expanded it to</div>
+            <div className={styles.w}>We automatically expanded it to</div>
             }
+            {hasTranslation &&
             <div className={styles.w}>We also search for</div>
+            }
           </div>
 
           <div className={styles.leftBox}>
-            {hasExpansion && labels.map(item => (
-              <div key={item.id}>
+            {hasExpansion && expands.map((item, index) => {
+              const key = `${item}_${index}`;
+              return <div key={key}>
                 <div>
                   <span className={styles.rightbox}>
                     <Checkbox
-                      checked={item.id === defaultExpansionChecked || currentExpansionChecked === item.id}
-                      onChange={this.onExpandedTermChange.bind(this, item.id)}
-                    >{item.expansion}
+                      checked={defaultExpansionChecked === index + 1 ||
+                      currentExpansionChecked === index + 1}
+                      onChange={this.onExpandedTermChange.bind(this, index)}
+                    >{item.word}
                     </Checkbox>
                   </span>
                 </div>
-
+                {hasTranslation &&
                 <div><span className={styles.rightbox}>
                   <Checkbox
-                    checked={currentTranslationChecked === item.id}
-                    onChange={this.onTranslationChange.bind(this, item.id)}
-                  >{item.translated}
+                    checked={currentTranslationChecked === index + 1 ? index + 1 : 0}
+                    onChange={this.onTranslationChange.bind(this)}
+                  >{item.word_zh}
                   </Checkbox>
-                 </span>
+                </span>
                 </div>
+                }
               </div>
-            ))}
+            })}
           </div>
-          <div onClick={this.onToggleClick} >more</div>
+          {hasExpansion &&
+          <div onClick={this.onToggleClick} className={styles.paddingLeftButton}>
+            <Button type="primary" size="small">More
+            </Button>
+          </div>
+          }
         </div>
 
         <div className={styles.box1}>
           <div className={styles.ww}>
             <span>Expand by knowledge graph</span>
             <span className={styles.paddingLeft}>
-            <Checkbox
-              onChange={this.onCheckAllChange}
-              indeterminate={indeterminate}
-              checked={checkAll} />
-          </span>
+              <Checkbox
+                onChange={this.onCheckAllChange}
+                indeterminate={indeterminate}
+                checked={checkAll} />
+            </span>
           </div>
-          <Checkbox.Group onChange={this.onKGChange}>
-            {superordinateWord && superordinateWord.map((opt, index) => {
-              const key = `${opt.label}_${index}`;
-              return <Checkbox key={key} value={opt.label}>
-                <span className={styles.suporordinateWord}>{opt.label}</span>
-              </Checkbox>;
-            })}
-            {subordinateWord && subordinateWord.map((opt, index) => {
-              const key = `${opt.label}_${index}`;
-              return <Checkbox key={key} value={opt.label}>
-                <span
-                className={styles.subordinateWord}>{opt.label}
-                </span>
-              </Checkbox>;
-            })}
+          <Checkbox.Group onChange={this.onKGChange} value={this.state.checkedList}>
+          {kgHypernym && kgHypernym.map((opt, index) => {
+            const key = `${opt.word}_${index}`;
+            return (<Checkbox key={key} value={this.state.checkedList}
+                              checked={checkAll} onChange={this.onKGChange}>
+              <span className={styles.suporordinateWord}>{opt.word}</span>
+            </Checkbox>);
+          })}
+          {kgHyponym && kgHyponym.map((opt, index) => {
+            const key = `${opt.word}_${index}`;
+            return (<Checkbox key={key} value={opt.word} checked={checkAll}>
+              <span className={styles.subordinateWord}>{opt.word}</span>
+            </Checkbox>);
+          })}
           </Checkbox.Group>
         </div>
       </div>
     );
   }
-
-
+}
+SearchHelp.propTypes = {
+  // labels: PropTypes.object.isRequired,
+  disableSmartSuggest: PropTypes.bool,
 };

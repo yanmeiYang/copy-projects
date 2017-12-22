d2000f27fba357f2a8138520850603ae731d44ab
elivoa <elivoa@gmail.com>
2017-12-22 12:00:52 +0800

    科协系统，第三方登录没验证。上线。
    Signed-off-by: elivoa <elivoa@gmail.com>

--- src/routes/auth/3rd-login/Simple3rdLogin.js
+++ src/routes/auth/3rd-login/Simple3rdLogin.js
@@ -9,8 +9,10 @@ import * as authUtil from 'utils/auth';
 import queryString from 'query-string';
 import { routerRedux } from 'dva/router';
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyLUZLaE1WZjR5YjR0SmFTMVNXRzlRd0dOQnhRMlZmZnI3SWNBU2g1ajRIWWN1RXZtenFvbHQ4T05ZRUdSUzlIbnhBWVJMczhUT0VXK1dGQ3pueURcL0lJamVobzNLTkZHZHBET3B5OXpEamQyeStzZE9EaHdBMiIsImlzcyI6InBsYXktYW5ndWxhci1zaWxob3VldHRlIiwiZXhwIjoxNTA2MTQ5MjE1LCJpYXQiOjE1MDM1NTcyMTUsImp0aSI6IjhiZTAyZTNkMGFhNTIzN2JiNjhlZTE1MGMyNzVkMTRlN2JhZmJmOWFmOGQ4MmM2NmY1MDBjNzgzZWZlYzc5NzZlOTlmYTIxODc0ODRmNDVhMDU5NjRmZWI1ZGY1YjM5OWEwNWE1OWU1OGU1MmU5OGZjYmE5NzViZjAwY2YwYTVlMWI4ODk5NGJiNDM1YjRhYmYzNTE4ZmFmNzc4MDg0MTc2NTNhOWE2OTFjOGU5NGJiYWVhNGIwODc0MzAyNjljMDFkYWUzMzcwNTA4MmZiODc1NTBjMzEyYWUzMGM1ZTFiNjdlZjIwMTQ0YjkzMDljZTgwNjYxMGM2MTVhNDM0NWEifQ.xvAw1Dw_cGnUUtB1fukV1bFwhUuzV2rtQQRfvJ3hVaI';
+// Token of elivoa scei_超级管理员
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyLTlBdk94XC9BV0JEVHVTXC9QQ29RdmZaWEd1QndES3VWNDVlS2UzOUt6cmtnVDUybVdyTG9qZjM5QW9oZUJneElkc3RLYmhBdWV6V3pZM1hKZklDMUV3dWV0RmdaZ1VDYkdHclZiOTVMM2tmZ21TcGc9PSIsInVpZCI6IjVhMjdjMDlkOWVkNWRiZTA2ZGQyNDIyNSIsInNyYyI6InNjZWkiLCJpc3MiOiJhcGkuYW1pbmVyLm9yZyIsImV4cCI6MTUxNjUwNTIzOSwiaWF0IjoxNTEzOTEzMjM5LCJqdGkiOiJhZGRhYzc2YTRkZmVjYmJhOWQ5OWMxMDVhNzA4MjZmMDljMmVkNWNiODQzZjUxOGRkMDg2NTRlNDZhMDhkNDQwMjI0ZjU5NzkwY2E0ZGZmMjc4MjM1ZTY1YjE1OWVkN2QyNjU2MTA2OGExMTc1MGNjNWJhM2RlYzVkODJlYTkxMzNiMDY2Mjg0NjEwNmU2NTQ4OWQxYTJlOGIyNmRhMDI4NzEyOGI0NWMxMDBiZjBkMTE0MjI3MGI2ODEzMzU2MzUxNWY1NTE2Yjk1YWM3NTIxYzQ0OGI3MzUyOTdiNTFkOTVkZTcxNzgyMmQwYTk0NjAyZmZhZjI2MjgzZTYzY2RiIiwiZW1haWwiOiJlbGl2b2FAZ21haWwuY29tIn0.XlEnpBs4G-aZBsvQp-EMwyXDzrps38ri1g1Xm0ReY_Q';
 const DEBUG_LOG = true;
+
 // Example:
 // http://localhost:8000/login/s3?n=%E9%99%B6%E6%B4%B5&a=15251664207&o=%E5%8C%97%E4%BA%AC%E8%8D%9F%E8%B6%A3%E4%BF%A1%E6%81%AF%E6%8A%80%E6%9C%AF%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8&pos=%E7%BB%8F%E7%90%86&p=15251664207&id=25
 // 说明：
@@ -35,20 +37,22 @@ export default class Simple3rdLogin extends Component {
     // position: 1,
     // isRegister: false,
 
-    checkPass: true,
+    step: '', // [empty | init, check_pass, failed, success ]
+    redirectUrl: '',
   };
 
   componentWillMount() {
     const { dispatch, location } = this.props;
-    const { n, a, o, pos, p, id, role, c } = queryString.parse(location.search);
+    const { n, a, o, pos, p, id, role, c, src } = queryString.parse(location.search);
+    this.setState({ redirectUrl: src || '/' });
 
     // [STEP 1] 获取URL中的参数，接收到的用户. TODO 验证URL是否合法，是否被人串改.
     const receivedUser = {
       email: `${a}@scei.com`,
       password: this.passwdgen(a),
-      first_name: n,
+      first_name: '',
       last_name: n,
-      position: pos,
+      // position: pos,
       // TODO save phone.
     };
     if (DEBUG_LOG) {
@@ -60,12 +64,16 @@ export default class Simple3rdLogin extends Component {
     if (DEBUG_LOG) {
       console.log('[STEP 2] Checksum: Skip...');
     }
+
     if (!checkPass) {
+      this.setState({ step: 'check_failed' });
       return;
     }
 
-    // const email = receivedUser.email;
-    const email = 'elivoa@gmail.com'; // DEBUG ONLY
+    this.setState({ step: 'check_pass' });
+
+    const { email } = receivedUser;
+    // const email = 'elivoa@gmail.com'; // DEBUG ONLY
 
     // [STEP 3] 是否已经登录，如果已经登录，验证ID是否一样。
     // 如果一样，则直接通过。如果不一样。则使用新的重新验证。
@@ -96,23 +104,23 @@ export default class Simple3rdLogin extends Component {
             console.log('[STEP 4.1] Create user: ', newUser);
           }
           dispatch({ type: 'auth/create3rdUser', payload: { ...newUser, token } })
-            .then((success) => {
-              console.log('create user result: ', success);
-              // success TODO ........
-
+            .then((createUserSuccess) => {
+              console.log('create user result: ', createUserSuccess);
+              // success TODO ........ 这块流程还可以优化呀呀呀。再看看。
+              // 创建成功，去登录.
+              return this.goLogin(email, receivedUser.password, role);
             })
             .catch((err) => {
               if (DEBUG_LOG) {
                 console.log('[STEP 4.1] Create user failed!', err);
               }
+              this.setState({ step: 'check_failed' });
               return false;
             });
         } else {
-          // 如果email存在，认为登录成功。
+          // 如果email存在，认为，使用账号登录。然后跳转。
           const { password } = receivedUser; // '123123';
-          dispatch({ type: 'app/login', payload: { email, password, role } });
-
-          return this.redirectSuccess();
+          return this.goLogin(email, password, role);
         }
       });
   }
@@ -133,8 +141,15 @@ export default class Simple3rdLogin extends Component {
 
   passwdgen = id => `data${id}mining`;
 
+  goLogin = (email, password, role) => {
+    this.props.dispatch({ type: 'app/login', payload: { email, password, role } });
+    return this.redirectSuccess();
+  };
+
   redirectSuccess = () => {
+    const { redirectUrl } = this.state;
     console.log('+++ success login, redirect 88 ');
+    this.props.dispatch(routerRedux.push({ pathname: redirectUrl }));
   };
 
   // componentWillUpdate(nextProps, nextState) {
@@ -158,8 +173,22 @@ export default class Simple3rdLogin extends Component {
   // }
 
   render() {
+    let message;
+    switch (this.state.step) {
+      case 'check_pass':
+        message = '验证成功';
+        break;
+      case 'check_failed':
+        message = '登录验证失败!';
+        break;
+      case 'success':
+        message = '登录验证成功! ';
+        break;
+      default:
+        message = '正在验证...';
+    }
     return (
-      <div>{this.state.checkPass ? '正在验证...' : '登录验证失败!'}</div>
+      <div>{message}</div>
     );
   }
 }
